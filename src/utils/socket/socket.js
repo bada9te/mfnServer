c48125465d993b4d11854650c58cc13b5de1fd77
bada9te <bogdantelep@gmail.com>
2024-02-01 20:44:31 +0200

    test msg create event handler

--- src/utils/socket/socket.js
+++ src/utils/socket/socket.js
@@ -3,20 +3,29 @@ const notificationsModel = require('../../models/notifications/notifications.mod
 const usersModel = require('../../models/users/users.model');
 const config = require('../../config');
 
+// get current connections 
+const getCurrentUsers = (io) => {
+    const users = [];
+    for (let [id, socket] of io.of("/").sockets) {
+        users.push({
+            socketId: id,
+            userId: socket.userId,
+        });
+    }
+    return users;
+}
+
+
 // socket.io
 const initSocketIO = async(SERVER) => {
     console.log(`[SOCKET] [*] Initializing...`);
     const io = new Server(SERVER, { cors: { origin: config.base.clientBase } });
+
+    // main
     io.on("connection", (socket) => {
         console.log(`[SOCKET] [+] User is connected, ${JSON.stringify({ SID: socket.id, UID: socket.userId }, null)}`);
-        const users = [];
-        for (let [id, socket] of io.of("/").sockets) {
-            users.push({
-                socketId: id,
-                userId: socket.userId,
-            });
-        }
-        socket.emit("users", users);
+        const users = getCurrentUsers(io);
+        socket.emit('users', users);
         socket.broadcast.emit("user connected", {
             socketId: socket.id,
             userId: socket.userId,
@@ -29,6 +38,18 @@ const initSocketIO = async(SERVER) => {
             });
             console.log(`[SOCKET] [-] User disconnected, ${JSON.stringify({ SID: socket.id, UID: socket.userId }, null)}`);
         });
+
+
+        /***************** CHATS HANDLERS *****************/
+        // create
+        socket.on('message create', ({ message, toUsers }) => {
+            console.log(message, toUsers)
+            const users = getCurrentUsers(io).filter(i => toUsers.includes(i.userId));
+            socket.to(users.map(i => i.socketId)).emit('message create', {
+                message,
+            });
+        });
+        
     });
 
     // middleware to check for userId
