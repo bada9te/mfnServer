173b93c5fc9a8dfe628be76ee198600dfa087e7b
bada9te <bogdantelep@gmail.com>
2024-05-18 14:12:37 +0300

    fix

--- src/entities/chat-messages/chat-messages.module.ts
+++ src/entities/chat-messages/chat-messages.module.ts
@@ -3,11 +3,14 @@ import { ChatMessagesService } from './chat-messages.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatMessage, ChatMessageSchema } from './chat-messages.schema';
 import { ChatMessagesResolver } from './chat-messages.resolver';
+import { PostsService } from '../posts/posts.service';
+import { Post, PostSchema } from '../posts/posts.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: ChatMessage.name, schema: ChatMessageSchema }])
+    MongooseModule.forFeature([{ name: ChatMessage.name, schema: ChatMessageSchema }]),
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
   ],
-  providers: [ChatMessagesService, ChatMessagesResolver]
+  providers: [ChatMessagesService, ChatMessagesResolver, PostsService]
 })
 export class ChatMessagesModule {}
--- src/entities/comments/comments.module.ts
+++ src/entities/comments/comments.module.ts
@@ -3,11 +3,14 @@ import { CommentsService } from './comments.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Comment, CommentSchema } from './comments.schema';
 import { CommentsResolver } from './comments.resolver';
+import { PostsService } from '../posts/posts.service';
+import { Post, PostSchema } from '../posts/posts.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }])
+    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
   ],
-  providers: [CommentsService, CommentsResolver]
+  providers: [CommentsService, CommentsResolver, PostsService]
 })
 export class CommentsModule {}
