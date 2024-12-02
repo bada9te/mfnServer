7d3e43d6339f5731118869c49be5430bec99e503
bada9te <bogdantelep@gmail.com>
2024-12-03 00:46:58 +0200

    new playlists mutation

--- src/entities/playlists/dto/swicth-track.dto.ts
+++ src/entities/playlists/dto/swicth-track.dto.ts
@@ -1,5 +1,9 @@
-import { SwitchTrackInPlaylistInput } from "src/graphql/graphql.schema";
+import { SwitchTrackInPlaylistInput, SwitchTrackInPlaylistsInput } from "src/graphql/graphql.schema";
 
 export class SwitchTrackDto extends SwitchTrackInPlaylistInput {
+    
+}
+
+export class SwitchTracksDto extends SwitchTrackInPlaylistsInput {
     
 }
