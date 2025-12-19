a134eeebcca0c9622ffd2be5ed5124b3355712f8
Bohdan Teliepov <97344806+bada9te@users.noreply.github.com>
2025-12-18 23:31:09 -0700

    updates

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,28 @@
-import { Handler } from 'aws-lambda';
-import serverlessExpress from '@vendia/serverless-express';
+// api/index.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
+import serverlessExpress from '@vendia/serverless-express';
+import { Handler } from 'aws-lambda';
 
-let server: Handler;
+let cachedServer: Handler;
 
-async function bootstrapServer() {
+async function bootstrapServer(): Promise<Handler> {
   const expressApp = express();
-
   expressApp.use(cookieParser());
 
   const app = await NestFactory.create(AppModule, expressApp as any);
   app.enableCors({ origin: process.env.CLIENT_BASE, credentials: true });
-
   await app.init();
 
   return serverlessExpress({ app: expressApp });
 }
 
-export const handler = async (event, context) => {
-  if (!server) {
-    server = await bootstrapServer();
+// Vercel expects a default export
+export default async function handler(event, context) {
+  if (!cachedServer) {
+    cachedServer = await bootstrapServer();
   }
-  return server(event, context);
-};
+  return cachedServer(event, context);
+}
